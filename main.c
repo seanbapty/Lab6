1798bdfab444cde746223e66e6598dc4e6d4e8bd
Sean Bapty <C16Sean.Bapty@usafa.edu>
2014-11-24 22:01:48 -0700

    added basic functionality

--- main.c
+++ main.c
@@ -1,14 +1,30 @@
 #include <msp430.h>
 #include "lab6.h"
+//#include "moveRobot.c"
 
 void main(void) {
     WDTCTL = WDTPW|WDTHOLD;                 // stop the watchdog timer
-    initPWM();
-
+    initPWM();								// initialize ports and timer
+while(1){									// continuously perform required functionality sequence
 	moveForward();
-    __delay_cycles(5000000);
-    moveBackward();
+    stop();
 
-    while(1){ }
+    moveBackward();
+    stop();
+
+    hardRight();
+    stop();
+
+    hardLeft();
+    stop();
+
+    smallRight();
+    stop();
+
+    smallLeft();
+    stop();
+
+
+}
 
 } // end main
--- moveRobot.c
+++ moveRobot.c
@@ -7,59 +7,121 @@
 
 #include "lab6.h"
 
-#define L_EN	BIT0
-#define L_PWM	BIT2
-#define R_PWM	BIT3
-#define R_EN	BIT4
-
-#define MOTOR_PWM	( L_PWM | R_PWM )
-#define MOTOR_EN	( L_EN | R_EN )
-#define MOTOR_PINS 	( MOTOR_PWM | MOTOR_EN )
-
-
 void initPWM(){
-	//P1DIR |= BIT2;
-	//P1SEL |= BIT2;
 
-	/*
-	P2DIR |= BIT1;
-	P2SEL |= BIT1;
-	P2DIR |= BIT4;
-	P2SEL |= BIT4;
-	P2DIR |= BIT5;
-	P2SEL |= BIT5;
-	*/
+	P2DIR |= L_PWM;
+	P2SEL |= L_PWM;
+	P2DIR |= R_PWM;
+	P2SEL |= R_PWM;
 
-	P2DIR |= MOTOR_PINS;
-	P2SEL |= MOTOR_PWM;
+	L_SET_OUT;
+	L_FORWARD;
 
+	R_SET_OUT;
+	R_FORWARD;
+
+	R_SET_OUT;
+	R_DIS;
+	L_SET_OUT;
+	R_DIS;
 	//configure timers
 	TA1CTL |= TACLR; //clear timer
 	TA1CTL = ID_3 | TASSEL_2 | MC_1;// Use 1:8 presclar off MCLK
-	TA1CCR0= 0xFFFF;
+	TA1CCR0= 100;
 
-	TA1CCR1=0x8888;
-	TA1CCTL1 |= OUTMOD_7;
+	TA1CCR1=80;		//different from TA1CCR2 to make up for imbalances in the wheel
+	TA1CCTL1 |= OUTMOD_3;	//ratio of values obtained by guess and check
 
-	TA1CCR2=0x8888;
+	TA1CCR2=20;
 	TA1CCTL2 |= OUTMOD_7;
 
+
+	L_DIRECTION_SET;
+	R_DIRECTION_SET;
+}
+
+void stop(){
+	R_DIS;
+	L_DIS;
+	__delay_cycles(STANDARD_DELAY);
+
+}
+
+void go(){
+	R_EN;
+	L_EN;
+}
+
+void modForward(){
+	TA1CCR1=10;		//different from TA1CCR2 to make up for imbalances in the wheel
+	TA1CCTL1 |= OUTMOD_3;	//ratio of values obtained by guess and check
+
+	TA1CCR2=90;
+	TA1CCTL2 |= OUTMOD_7;
+}
+
+void modBackward(){
+	TA1CCR1=80;		//different from TA1CCR2 to make up for imbalances in the wheel
+	TA1CCTL1 |= OUTMOD_7;	//ratio of values obtained by guess and check
+
+	TA1CCR2=20;
+	TA1CCTL2 |= OUTMOD_3;
 }
 
 void moveForward(){
-	P2DIR |= MOTOR_EN;
-
-	__delay_cycles(800000);
-
-	P2DIR &= ~MOTOR_EN;
+	modForward();
+	go();
+	L_FORWARD;
+	R_FORWARD;
+	__delay_cycles(SHORT_DELAY);
+	//P2OUT &= ~MOTOR_EN;
 }
 
 void moveBackward(){
-	TA1CCTL1 |= OUTMOD_3;
-	TA1CCTL2 |= OUTMOD_3;
-	P2DIR |= MOTOR_EN;
-	__delay_cycles(800000);
-	P2DIR &= ~MOTOR_EN;
+	modBackward();
+	go();
+	L_BACKWARD;
+	R_BACKWARD;
+	__delay_cycles(STANDARD_DELAY);
+	//P2DIR &= ~MOTOR_EN;
 }
 
+void hardRight(){
+	modForward();
+	go();
+	L_BACKWARD;
+	R_FORWARD;
+	//P2DIR |= MOTOR_EN;
+	__delay_cycles(SHORT_DELAY);
+	//P2DIR &= ~MOTOR_EN;
+}
 
+void hardLeft(){
+	modForward();
+	go();
+	L_FORWARD;
+	R_BACKWARD;
+	//P2DIR |= MOTOR_EN;
+	__delay_cycles(STANDARD_DELAY);
+	//P2DIR &= ~MOTOR_EN;
+}
+
+void smallLeft(){
+	modForward();
+	go();
+	L_FORWARD;
+	R_BACKWARD;
+	//P2DIR |= MOTOR_EN;
+	__delay_cycles(SHORT_DELAY);
+	//P2DIR &= ~MOTOR_EN;
+}
+
+void smallRight(){
+	modForward();
+	go();
+	L_BACKWARD;
+	R_FORWARD;
+	//P2DIR |= MOTOR_EN;
+	__delay_cycles(SHORT_DELAY/2);//same as large turn for less time
+	//P2DIR &= ~MOTOR_EN;
+}
